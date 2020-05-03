a19873b512ca1c5fceb9a75cf2caf470d9b64492
Amila Abeygunasekara <auabey@gmail.com>
2020-05-03 13:49:15 +0530

    Add clear scan history confirmation dialog

--- QRScanner/app/src/main/java/com/amila/qrscanner/ScanHistoryActivity.java
+++ QRScanner/app/src/main/java/com/amila/qrscanner/ScanHistoryActivity.java
@@ -6,7 +6,10 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.amila.qrscanner.resultdb.Result;
@@ -18,12 +21,17 @@ import java.util.List;
 
 public class ScanHistoryActivity extends AppCompatActivity {
 
+    private static final String TAG = "ScanHistoryActivity";
+
     private ResultViewModel mResultViewModel;
+    private List<Result> mResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_history);
+
+        mResults = null;
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final ResultListAdapter adapter = new ResultListAdapter(this);
@@ -35,14 +43,41 @@ public class ScanHistoryActivity extends AppCompatActivity {
         mResultViewModel.getAllResults().observe(this, new Observer<List<Result>>() {
             @Override
             public void onChanged(List<Result> results) {
-                Collections.reverse(results);
-                adapter.setResults(results);
+                mResults = results;
+                Collections.reverse(mResults);
+                adapter.setResults(mResults);
             }
         });
     }
 
     public void clearDb(View view) {
-        mResultViewModel.clearAll();
+        if (mResults == null) {
+            Log.e(TAG, "clearDb: mResults is null");
+            return;
+        }
+
+        if (mResults.size() == 0) {
+            DialogInterface.OnClickListener listener = (dialog, id) -> finish();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("QR Code Reader")
+                    .setMessage(R.string.empty_scan_history)
+                    .setPositiveButton("OK", listener)
+                    .show();
+        } else {
+            DialogInterface.OnClickListener listener = (dialog, id) -> {
+                if (id == DialogInterface.BUTTON_POSITIVE) {
+                    mResultViewModel.clearAll();
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("QR Code Reader")
+                    .setMessage(R.string.clear_history_confirmation)
+                    .setPositiveButton("OK", listener)
+                    .setNegativeButton("Cancel", listener)
+                    .show();
+        }
     }
 
     public void back(View view) {
