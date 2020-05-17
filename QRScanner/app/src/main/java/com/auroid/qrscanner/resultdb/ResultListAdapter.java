3ec8750aeee2397fd9264cb528c3ee0962ca336a
Amila Abeygunasekara <auabey@gmail.com>
2020-05-17 14:45:17 +0530

    Display icons in scan history

--- QRScanner/app/src/main/java/com/auroid/qrscanner/resultdb/ResultListAdapter.java
+++ QRScanner/app/src/main/java/com/auroid/qrscanner/resultdb/ResultListAdapter.java
@@ -3,6 +3,7 @@ package com.auroid.qrscanner.resultdb;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.auroid.qrscanner.R;
 import com.auroid.qrscanner.serializable.BarcodeWrapper;
 
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.gson.Gson;
 
 import java.text.SimpleDateFormat;
@@ -54,12 +56,38 @@ public class ResultListAdapter extends ListAdapter<Result, ResultListAdapter.Res
     @Override
     public void onBindViewHolder(@NonNull ResultViewHolder holder, int position) {
         Result current = getItem(position);
-        holder.resultItemView.setText(fromJson(current.getResult()).displayValue);
+        BarcodeWrapper barcodeWrapper = mGson.fromJson(current.getResult(), BarcodeWrapper.class);
+        holder.resultItemView.setText(barcodeWrapper.displayValue);
         holder.timeItemView.setText(mFormatter.format(current.getTime()));
+        setIcon(barcodeWrapper.valueFormat, holder);
     }
 
-    private BarcodeWrapper fromJson(String resultJson) {
-        return mGson.fromJson(resultJson, BarcodeWrapper.class);
+    private void setIcon(int resultType, ResultViewHolder holder) {
+        switch(resultType) {
+            case Barcode.URL:
+                holder.iconItemView.setImageResource(R.drawable.ic_public_white_24dp);
+                break;
+
+            case Barcode.PHONE:
+                holder.iconItemView.setImageResource(R.drawable.ic_phone_white_24dp);
+                break;
+
+            case Barcode.GEO:
+                holder.iconItemView.setImageResource(R.drawable.ic_location_on_white_24dp);
+                break;
+
+            case Barcode.CALENDAR_EVENT:
+                holder.iconItemView.setImageResource(R.drawable.ic_calender_white_24dp);
+                break;
+
+            case Barcode.WIFI:
+                holder.iconItemView.setImageResource(R.drawable.ic_wifi_white_24dp);
+                break;
+
+            default:
+                holder.iconItemView.setImageResource(R.drawable.ic_text_white_24dp);
+                break;
+        }
     }
 
     public Result getResultAt(int position) {
@@ -69,11 +97,13 @@ public class ResultListAdapter extends ListAdapter<Result, ResultListAdapter.Res
     static class ResultViewHolder extends RecyclerView.ViewHolder {
         private final TextView resultItemView;
         private final TextView timeItemView;
+        private final ImageView iconItemView;
 
         private ResultViewHolder(View itemView) {
             super(itemView);
             resultItemView = itemView.findViewById(R.id.textViewResult);
             timeItemView = itemView.findViewById(R.id.textViewTime);
+            iconItemView = itemView.findViewById(R.id.imgview_history);
         }
     }
 }
