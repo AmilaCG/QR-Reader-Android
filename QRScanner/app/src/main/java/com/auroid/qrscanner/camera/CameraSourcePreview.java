1e36442882a6b7222415f175d0f140530da785eb
Amila Abeygunasekara <auabey@gmail.com>
2020-06-02 00:02:44 +0530

    Adding support for devices with long screens

--- QRScanner/app/src/main/java/com/auroid/qrscanner/camera/CameraSourcePreview.java
+++ QRScanner/app/src/main/java/com/auroid/qrscanner/camera/CameraSourcePreview.java
@@ -101,25 +101,32 @@ public class CameraSourcePreview extends FrameLayout {
             }
         }
 
-        // Match the width of the child view to its parent.
-        int childWidth = layoutWidth;
-        int childHeight = (int) (childWidth / previewSizeRatio);
-        if (childHeight <= layoutHeight) {
+        int childWidth = (int) (layoutHeight * previewSizeRatio);
+        int childHeight = (int) (layoutWidth / previewSizeRatio);
+
+        if (layoutWidth <= childWidth) {
+            // When the child view is too wide to be fitted in its parent:
+            int excessLenInHalf = (childWidth - layoutWidth) / 2;
             for (int i = 0; i < getChildCount(); ++i) {
-                getChildAt(i).layout(0, 0, childWidth, childHeight);
+                View childView = getChildAt(i);
+                if (childView.getId() == R.id.static_overlay_container) {
+                    getChildAt(i).layout(0, 0, layoutWidth, layoutHeight);
+                } else {
+                    childView.layout(-excessLenInHalf, 0, layoutWidth + excessLenInHalf, layoutHeight);
+                }
             }
         } else {
             // When the child view is too tall to be fitted in its parent: If the child view is static
-            // overlay view container (contains views such as bottom prompt chip), we apply the size of
-            // the parent view to it. Otherwise, we offset the top/bottom position equally to position it
+            // overlay view container (contains views such as guide chip), we apply the size of the
+            // parent view to it. Otherwise, we offset the top/bottom position equally to position it
             // in the center of the parent.
             int excessLenInHalf = (childHeight - layoutHeight) / 2;
             for (int i = 0; i < getChildCount(); ++i) {
                 View childView = getChildAt(i);
                 if (childView.getId() == R.id.static_overlay_container) {
-                    childView.layout(0, 0, childWidth, layoutHeight);
+                    childView.layout(0, 0, layoutWidth, layoutHeight);
                 } else {
-                    childView.layout(0, -excessLenInHalf, childWidth, layoutHeight + excessLenInHalf);
+                    childView.layout(0, -excessLenInHalf, layoutWidth, layoutHeight + excessLenInHalf);
                 }
             }
         }
